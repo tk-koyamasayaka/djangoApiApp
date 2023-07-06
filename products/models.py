f54255699f792c03d90649b3e29e486265e64df7
Sayaka Ishigaki <38.skrisyk@gmail.com>
2023-07-06 23:51:14 +0900

    マイグレーションテスト

--- products/models.py
+++ products/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Comment(models.Model):
+    name = models.CharField(max_length=200)
+    content = models.TextField(verbose_name='内容',blank=True,null=True,max_length=500)
