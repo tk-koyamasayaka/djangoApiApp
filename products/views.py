867aa4579ac773ffe0bf39ba31e6ad1968b8837c
Sayaka Ishigaki <38.skrisyk@gmail.com>
2023-07-08 23:57:21 +0900

    API作成 - first

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Comment
+from .serializers import CommentSerializer
+
 
 # Create your views here.
+# APIの処理を実装する
+# CRUDの処理を書く
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
