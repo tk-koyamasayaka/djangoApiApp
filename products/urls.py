6937cb6de10c0381e733bc6485e1847c84e0e11c
Sayaka Ishigaki <38.skrisyk@gmail.com>
2023-07-09 15:43:27 +0900

    dummy,list APIの作成

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CommentViewSet
+from .views import CommentViewSet, CommentDummyApiView, comment_list
 
 router = DefaultRouter()
 router.register('comments', CommentViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('dummy/', CommentDummyApiView.as_view()),
+    path('list/', comment_list),
 ]
--- products/views.py
+++ products/views.py
@@ -2,12 +2,28 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Comment
 from .serializers import CommentSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 
 # Create your views here.
 # APIの処理を実装する
 # CRUDの処理を書く
 
-class CommentViewSet(viewsets.ModelViewSet):
+class CommentViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+
+
+class CommentDummyApiView(APIView):
+    def get(self, request):
+        return Response({"name": "DUMMY"})
+
+
+@api_view(['GET'])
+def comment_list(request):
+    if request.method == 'GET':
+        comments = Comment.objects.all()
+        serializer = CommentSerializer(comments, many=True)  # many=True list形式の場合はまとめて処理できるようになる引数
+        return Response(serializer.data)
