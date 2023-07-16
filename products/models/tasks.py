71918dde057df5a2484bd7fcc7c0629ac9e913c7
Sayaka Ishigaki <38.skrisyk@gmail.com>
2023-07-16 23:27:17 +0900

    リファクタリング -　ディレクトリ構成変更

---
+++ products/models/tasks.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Task(models.Model):
+    content = models.TextField(verbose_name="内容", blank=False, null=False, max_length=500)
+    due_date = models.DateTimeField(verbose_name="期限日", null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)  # TODO: abstract classで定義する
+    update_at = models.DateTimeField(auto_now_add=True)  # TODO: abstract classで定義する
--- products/views.py
+++ products/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Comment
-from .serializers import CommentSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -11,19 +9,3 @@ from rest_framework.decorators import api_view
 # APIの処理を実装する
 # CRUDの処理を書く
 
-class CommentViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
-
-
-class CommentDummyApiView(APIView):
-    def get(self, request):
-        return Response({"name": "DUMMY"})
-
-
-@api_view(['GET'])
-def comment_list(request):
-    if request.method == 'GET':
-        comments = Comment.objects.all()
-        serializer = CommentSerializer(comments, many=True)  # many=True list形式の場合はまとめて処理できるようになる引数
-        return Response(serializer.data)
